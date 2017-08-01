64e284f3f037b1af7018ef86f28a1c0f1ec9d77b
wangmiao1218 <wangmiao1218@163.com>
2017-08-01 18:05:34 +0800

    add zy、tjzx、sq controller

--- crf_mysql/src/main/java/com/gennlife/info/controller/AddCrfTemplateAnzhenController.java
+++ crf_mysql/src/main/java/com/gennlife/info/controller/AddCrfTemplateAnzhenController.java
@@ -55,7 +55,7 @@ public class AddCrfTemplateAnzhenController{
 				else {
 					if ("调查时间".contains(jbxxList.get(i).getChineseName())) {
 						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
-						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[6]/a").click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[2]/a").click();
 					}
 					else if ("出生日期".contains(jbxxList.get(i).getChineseName())) {
 						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
@@ -95,16 +95,15 @@ public class AddCrfTemplateAnzhenController{
 		return "redirect:/page/ok.html";
 	}
 	
-	
 	/** 
-	* @Title: addCrfTemplateAnzhen_Mz_Jz 
-	* @Description: 添加门诊_门诊-1（就诊-xxx等所有信息）
+	* @Title: addCrfTemplateAnzhen_MzWithoutHzxx 
+	* @Description: 添加门诊_门诊-1（门诊下面所有信息，无随访）
 	* @param: @throws Exception :
 	* @return: String
 	* @throws 
 	*/
-	@RequestMapping("addCrfTemplateAnzhen_Mz_Jz")
-	public String addCrfTemplateAnzhen_Mz_Jz() throws Exception {
+	@RequestMapping("addCrfTemplateAnzhen_MzWithoutHzxx")
+	public String addCrfTemplateAnzhen_MzWithoutHzxx() throws Exception {
 		// 登录并到add页面
 		PhantomJSDriver driver = CreateWebDriver.createWebDriverByPhantomJSDriver();
 		String value = LoginCrfOfAnzhen.loginByPhantomJSDriver(driver);
@@ -139,10 +138,10 @@ public class AddCrfTemplateAnzhenController{
 			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_13_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－CT检查"));
 			
 			//就诊－眼底检查
-			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_14_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－眼底检查"));
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_14_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－眼底检查"));
 			
 			//就诊－多导睡眠图(PSG)监测
-			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_15_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－多导睡眠图(PSG)监测"));
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_15_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－多导睡眠图(PSG)监测"));
 			
 			//就诊－实验室检验
 			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_16_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－实验室检验"));
@@ -157,6 +156,385 @@ public class AddCrfTemplateAnzhenController{
 
 		return "redirect:/page/ok.html";
 	}
+	
+	
+	/** 
+	* @Title: addCrfTemplateAnzhen_Zy_Hzxx 
+	* @Description: 添加住院_患者信息(包括基本信息、个人病史、家族史、生活方式、本次入院前2周内药物治疗史)
+	* @param: @throws Exception :
+	* @return: String
+	* @throws 
+	*/
+	@RequestMapping("addCrfTemplateAnzhen_Zy_Hzxx")
+	public String addCrfTemplateAnzhen_Zy_Hzxx() throws Exception {
+		// 获取所有基本信息list
+		List<CrfTemplateAnzhen> jbxxList = crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("基本信息");
+
+		// 登录并到add页面
+		PhantomJSDriver driver = CreateWebDriver.createWebDriverByPhantomJSDriver();
+		String value = LoginCrfOfAnzhen.loginAndToAddOfZhuYuanAndBasicInfoByPhantomJSDriver(driver);
+		
+		if ("添加页面".contains(value)) {
+			// 循环jbxxList
+			for (int i = 0; i < jbxxList.size(); i++) {
+				if ("枚举型".contains(jbxxList.get(i).getVariableType())) {
+					new Select(driver.findElementById(jbxxList.get(i).getIdXpath())).selectByValue(jbxxList.get(i).getInputValue());
+				}
+				else {
+					if ("调查时间".contains(jbxxList.get(i).getChineseName())) {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[2]/a").click();
+					}
+					else if ("出生日期".contains(jbxxList.get(i).getChineseName())) {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/div/div/select[1]").click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/div/div/select[1]/option[82]").click();//1998
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[6]/a").click();
+					}
+					else {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).clear();
+						driver.findElementById(jbxxList.get(i).getIdXpath()).sendKeys(jbxxList.get(i).getInputValue());
+					}
+				}
+			}
+			
+			//循环后保存
+			driver.findElementById("input-save").click();
+			Thread.sleep(1000);
+			driver.findElementByClassName("u-btn").click();
+			Thread.sleep(1000);
+			
+			//个人病史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_3_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("个人病史"));
+			
+			//家族史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_4_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("家族史"));
+			
+			//生活方式
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_5_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("生活方式"));
+			
+			//本次入院前2周内药物治疗史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_6_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("本次入院前2周内药物治疗史"));
+		}
+
+		// 关闭driver
+		QuitWebDriver.quitWebDriverByPhantomJSDriver(driver);
+
+		return "redirect:/page/ok.html";
+	}
+	
+	/** 
+	* @Title: addCrfTemplateAnzhen_ZyWithoutHzxx 
+	* @Description: 添加住院（住院下面所有信息，无随访）
+	* @param: @throws Exception :
+	* @return: String
+	* @throws 
+	*/
+	@RequestMapping("addCrfTemplateAnzhen_ZyWithoutHzxx")
+	public String addCrfTemplateAnzhen_ZyWithoutHzxx() throws Exception {
+		// 登录并到add页面
+		PhantomJSDriver driver = CreateWebDriver.createWebDriverByPhantomJSDriver();
+		String value = LoginCrfOfAnzhen.loginByPhantomJSDriver(driver);
+		
+		if ("登陆成功".contains(value)) {
+			//已有基本信息的病例xpath
+			driver.findElementByXPath(".//*[@id='case-list-container']/tbody/tr[2]/td[2]/a").click();
+			// 得到当前窗口的set集合
+			Set<String> winHandels = driver.getWindowHandles();
+			// 将set集合存入list对象
+			List<String> it = new ArrayList<String>(winHandels);
+			// 切换到弹出的新窗口
+			driver.switchTo().window(it.get(1));
+			Thread.sleep(2000);
+			
+			//就诊－体格检查
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_8_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－体格检查"));
+			
+			//就诊－24小时动态血压监测
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_9_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－24小时动态血压监测"));
+			
+			//就诊－四肢血压及动脉弹性检查
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_10_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－四肢血压及动脉弹性检查"));
+			
+			//就诊－电生理检查
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_11_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－电生理检查"));
+			
+			//就诊－超声检查
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_12_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－超声检查"));
+			
+			//就诊－CT检查
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_13_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－CT检查"));
+			
+			//就诊－眼底检查
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_14_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－眼底检查"));
+			
+			//就诊－多导睡眠图(PSG)监测
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_15_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－多导睡眠图(PSG)监测"));
+			
+			//就诊－实验室检验
+			//AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_16_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－实验室检验"));
+			
+			//就诊－住院与诊断
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_17_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－住院与诊断"));
+			
+		}
+
+		// 关闭driver
+		QuitWebDriver.quitWebDriverByPhantomJSDriver(driver);
+
+		return "redirect:/page/ok.html";
+	}
+	
+
+	/** 
+	* @Title: addCrfTemplateAnzhen_Tjzx_Hzxx 
+	* @Description: 添加体检中心_患者信息(包括基本信息、个人病史、家族史、生活方式、本次入院前2周内药物治疗史)
+	* @param: @throws Exception :
+	* @return: String
+	* @throws 
+	*/
+	@RequestMapping("addCrfTemplateAnzhen_Tjzx_Hzxx")
+	public String addCrfTemplateAnzhen_Tjzx_Hzxx() throws Exception {
+		// 获取所有基本信息list
+		List<CrfTemplateAnzhen> jbxxList = crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("基本信息");
+
+		// 登录并到add页面
+		PhantomJSDriver driver = CreateWebDriver.createWebDriverByPhantomJSDriver();
+		String value = LoginCrfOfAnzhen.loginAndToAddOfTiJianAndBasicInfoByPhantomJSDriver(driver);
+		
+		if ("添加页面".contains(value)) {
+			// 循环jbxxList
+			for (int i = 0; i < jbxxList.size(); i++) {
+				if ("枚举型".contains(jbxxList.get(i).getVariableType())) {
+					new Select(driver.findElementById(jbxxList.get(i).getIdXpath())).selectByValue(jbxxList.get(i).getInputValue());
+				}
+				else {
+					if ("调查时间".contains(jbxxList.get(i).getChineseName())) {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[2]/a").click();
+					}
+					else if ("出生日期".contains(jbxxList.get(i).getChineseName())) {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/div/div/select[1]").click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/div/div/select[1]/option[82]").click();//1998
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[6]/a").click();
+					}
+					else {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).clear();
+						driver.findElementById(jbxxList.get(i).getIdXpath()).sendKeys(jbxxList.get(i).getInputValue());
+					}
+				}
+			}
+			
+			//循环后保存
+			driver.findElementById("input-save").click();
+			Thread.sleep(1000);
+			driver.findElementByClassName("u-btn").click();
+			Thread.sleep(1000);
+			
+			//个人病史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_3_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("个人病史"));
+			
+			//家族史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_4_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("家族史"));
+			
+			//生活方式
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_5_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("生活方式"));
+			
+			//本次入院前2周内药物治疗史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_6_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("本次入院前2周内药物治疗史"));
+		}
+
+		// 关闭driver
+		QuitWebDriver.quitWebDriverByPhantomJSDriver(driver);
+
+		return "redirect:/page/ok.html";
+	}
+	
+	/** 
+	* @Title: addCrfTemplateAnzhen_TjzxWithoutHzxx 
+	* @Description: 添加体检中心（体检中心下面所有信息，无随访）
+	* @param: @throws Exception :
+	* @return: String
+	* @throws 
+	*/
+	@RequestMapping("addCrfTemplateAnzhen_TjzxWithoutHzxx")
+	public String addCrfTemplateAnzhen_TjzxWithoutHzxx() throws Exception {
+		// 登录并到add页面
+		PhantomJSDriver driver = CreateWebDriver.createWebDriverByPhantomJSDriver();
+		String value = LoginCrfOfAnzhen.loginByPhantomJSDriver(driver);
+		
+		if ("登陆成功".contains(value)) {
+			//已有基本信息的病例xpath
+			driver.findElementByXPath(".//*[@id='case-list-container']/tbody/tr[5]/td[2]/a").click();
+			// 得到当前窗口的set集合
+			Set<String> winHandels = driver.getWindowHandles();
+			// 将set集合存入list对象
+			List<String> it = new ArrayList<String>(winHandels);
+			// 切换到弹出的新窗口
+			driver.switchTo().window(it.get(1));
+			Thread.sleep(2000);
+			
+			//就诊－体格检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_8_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－体格检查"));
+			
+			//就诊－24小时动态血压监测
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_9_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－24小时动态血压监测"));
+			
+			//就诊－四肢血压及动脉弹性检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_10_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－四肢血压及动脉弹性检查"));
+			
+			//就诊－电生理检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_11_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－电生理检查"));
+			
+			//就诊－超声检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_12_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－超声检查"));
+			
+			//就诊－CT检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_13_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－CT检查"));
+			
+			//就诊－眼底检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_14_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－眼底检查"));
+			
+			//就诊－多导睡眠图(PSG)监测
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_15_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－多导睡眠图(PSG)监测"));
+			
+			//就诊－实验室检验
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_16_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－实验室检验"));
+			
+		}
+
+		// 关闭driver
+		QuitWebDriver.quitWebDriverByPhantomJSDriver(driver);
+
+		return "redirect:/page/ok.html";
+	}
+	
+	
+	/** 
+	* @Title: addCrfTemplateAnzhen_Sq_Hzxx 
+	* @Description: 添加社区_患者信息(包括基本信息、个人病史、家族史、生活方式、本次入院前2周内药物治疗史)
+	* @param: @throws Exception :
+	* @return: String
+	* @throws 
+	*/
+	@RequestMapping("addCrfTemplateAnzhen_Sq_Hzxx")
+	public String addCrfTemplateAnzhen_Sq_Hzxx() throws Exception {
+		// 获取所有基本信息list
+		List<CrfTemplateAnzhen> jbxxList = crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("基本信息");
+
+		// 登录并到add页面
+		PhantomJSDriver driver = CreateWebDriver.createWebDriverByPhantomJSDriver();
+		String value = LoginCrfOfAnzhen.loginAndToAddOfSheQuAndBasicInfoByPhantomJSDriver(driver);
+		
+		if ("添加页面".contains(value)) {
+			// 循环jbxxList
+			for (int i = 0; i < jbxxList.size(); i++) {
+				if ("枚举型".contains(jbxxList.get(i).getVariableType())) {
+					new Select(driver.findElementById(jbxxList.get(i).getIdXpath())).selectByValue(jbxxList.get(i).getInputValue());
+				}
+				else {
+					if ("调查时间".contains(jbxxList.get(i).getChineseName())) {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[2]/a").click();
+					}
+					else if ("出生日期".contains(jbxxList.get(i).getChineseName())) {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/div/div/select[1]").click();
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/div/div/select[1]/option[82]").click();//1998
+						driver.findElementByXPath(".//*[@id='ui-datepicker-div']/table/tbody/tr[1]/td[6]/a").click();
+					}
+					else {
+						driver.findElementById(jbxxList.get(i).getIdXpath()).clear();
+						driver.findElementById(jbxxList.get(i).getIdXpath()).sendKeys(jbxxList.get(i).getInputValue());
+					}
+				}
+			}
+			
+			//循环后保存
+			driver.findElementById("input-save").click();
+			Thread.sleep(1000);
+			driver.findElementByClassName("u-btn").click();
+			Thread.sleep(1000);
+			
+			//个人病史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_3_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("个人病史"));
+			
+			//家族史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_4_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("家族史"));
+			
+			//生活方式
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_5_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("生活方式"));
+			
+			//本次入院前2周内药物治疗史
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_6_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("本次入院前2周内药物治疗史"));
+		}
+
+		// 关闭driver
+		QuitWebDriver.quitWebDriverByPhantomJSDriver(driver);
+
+		return "redirect:/page/ok.html";
+	}
+	
+	/** 
+	* @Title: addCrfTemplateAnzhen_SqWithoutHzxx 
+	* @Description: 添加社区（社区下面所有信息，无随访）
+	* @param: @throws Exception :
+	* @return: String
+	* @throws 
+	*/
+	@RequestMapping("addCrfTemplateAnzhen_SqWithoutHzxx")
+	public String addCrfTemplateAnzhen_SqWithoutHzxx() throws Exception {
+		// 登录并到add页面
+		PhantomJSDriver driver = CreateWebDriver.createWebDriverByPhantomJSDriver();
+		String value = LoginCrfOfAnzhen.loginByPhantomJSDriver(driver);
+		
+		if ("登陆成功".contains(value)) {
+			//已有基本信息的病例xpath
+			driver.findElementByXPath(".//*[@id='case-list-container']/tbody/tr[6]/td[2]/a").click();
+			// 得到当前窗口的set集合
+			Set<String> winHandels = driver.getWindowHandles();
+			// 将set集合存入list对象
+			List<String> it = new ArrayList<String>(winHandels);
+			// 切换到弹出的新窗口
+			driver.switchTo().window(it.get(1));
+			Thread.sleep(2000);
+			
+			//就诊－体格检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_8_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－体格检查"));
+			
+			//就诊－24小时动态血压监测
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_9_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－24小时动态血压监测"));
+			
+			//就诊－四肢血压及动脉弹性检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_10_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－四肢血压及动脉弹性检查"));
+			
+			//就诊－电生理检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_11_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－电生理检查"));
+			
+			//就诊－超声检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_12_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－超声检查"));
+			
+			//就诊－CT检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_13_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－CT检查"));
+			
+			//就诊－眼底检查
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_14_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－眼底检查"));
+			
+			//就诊－多导睡眠图(PSG)监测
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_15_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－多导睡眠图(PSG)监测"));
+			
+			//就诊－实验室检验
+			AnzhenMethodByDriverAndIdXpath.globalMethod_Enum_MultiSelect(driver, "crf-data-tree_16_span", crfTemplateAnzhenService.getCrfTemplateAnzhenListByBaseName("就诊－实验室检验"));
+			
+		}
+
+		// 关闭driver
+		QuitWebDriver.quitWebDriverByPhantomJSDriver(driver);
+
+		return "redirect:/page/ok.html";
+	}
+	
 	
 	
 }

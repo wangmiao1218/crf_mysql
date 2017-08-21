7983f1020ada84749ccb6c3ad458732f889113bc
wangmiao1218 <wangmiao1218@163.com>
2017-08-21 18:03:45 +0800

    增加fileUtils工具类中，替换文件中字符串方法

--- crf_mysql/src/test/java/com/gennlife/crf/utils/test/TestFileUtils.java
+++ crf_mysql/src/test/java/com/gennlife/crf/utils/test/TestFileUtils.java
@@ -7,24 +7,28 @@ import org.junit.Test;
 import com.gennlife.crf.utils.FileUtils;
 
 public class TestFileUtils {
-	private static String fileName="E:\\安贞\\_wm数据json&js\\21100003_test.json";
-	private static String fileName1="E:\\安贞\\_wm数据json&js\\1.json";
-	private static String fileName2="E:\\安贞\\_wm数据json&js\\1_3.json";
 
 	@Test
 	public void fortest() throws Exception {
+		String fileName1="E:\\安贞\\_wm数据json&js\\安贞3w数据_北京长庚_无图片\\2.json";
 		File oldfile = new File(fileName1);
-		File newFlie = new File(fileName2);
-		
-		String returnValue = FileUtils.readFileAndReturnValue(oldfile, "21166666");
-		String substring = returnValue.replace("21166666", "21177777");
-		
-		FileUtils.readFileAndReplaceStrToNewFile(oldfile, returnValue, substring, newFlie);
+		String returnValue = FileUtils.readFileAndReturnValue(oldfile, "13100002");
+		String returnValue2 = FileUtils.readFileAndReturnValue(oldfile, "38cca9c1-0830-4518-99d4-b74a58c9b90515dbbc2e934");
+
+		for (int i = 1; i < 30001; i++) {
+			String substring = returnValue.replace("13100002", 13110000+i+"");
+			File newFile = new File("E:\\安贞\\_wm数据json&js\\安贞3w数据_北京长庚_无图片\\2_"+i+".json");
+			FileUtils.readFileAndReplaceStrToNewFile(oldfile, returnValue, substring, newFile);
+			
+			String substring2 = returnValue2.replace("38cca9c1-0830-4518-99d4-b74a58c9b90515dbbc2e934", 13110000+i+"-0830-4518-99d4-b74a58c9b90515dbbc2e934");
+			FileUtils.readFileAndReplaceStrToNewFile(newFile, returnValue2, substring2, newFile);
+		}
 		
 	}
 	
 	@Test
 	public void readFileAndReturnValue() throws Exception {
+		String fileName="E:\\安贞\\_wm数据json&js\\安贞3w数据_不同id\\1.json";
 		File file = new File(fileName);
 		String returnValue = FileUtils.readFileAndReturnValue(file, "21166666");
 		System.out.println(returnValue);
@@ -36,6 +40,9 @@ public class TestFileUtils {
 	
 	@Test
 	public void readFileAndReplaceStrToNewFile() throws Exception {
+		String fileName1="E:\\安贞\\_wm数据json&js\\安贞3w数据_不同id\\1.json";
+		String fileName2="E:\\安贞\\_wm数据json&js\\1_1.json";
+		
 		File oldfile = new File(fileName1);
 		File newFlie = new File(fileName2);
 		

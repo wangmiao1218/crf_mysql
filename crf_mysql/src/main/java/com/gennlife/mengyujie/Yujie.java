50d9ba7b247cf968a51a4bfc0a8b60a0f3b1f8cc
wangmiao1218 <wangmiao1218@163.com>
2017-08-29 18:18:26 +0800

    test

--- crf_mysql/src/main/java/com/gennlife/mengyujie/Yujie.java
+++ crf_mysql/src/main/java/com/gennlife/mengyujie/Yujie.java
@@ -47,27 +47,4 @@ public class Yujie{
 	}
 	
 	
-	/** 
-	* @Title: readTwoContentAndJudgeAndWriteResult 
-	* @Description: 读excel两列值，并比较是否相等，在对应列写入比较结果，pass为相等，no为不相等
-	* @param: @param excel ：出入的excel
-	* @param: @param allRow  ：总共需要比较的行数
-	* @param: @param beginCell  ：需要比较的列1（从0开始）
-	* @param: @param endCell  ：需要比较的列2（从0开始）
-	* @param: @param writeCell  ：写入结果的列（从0开始）
-	* @param: @return :
-	* @return: String
-	* @throws 
-	*/
-	public static String readTwoContentAndJudgeAndWriteResult(Excel excel, Integer allRow, Integer beginCell,Integer endCell,Integer writeCell ) {
-		for (int i = 0; i < allRow; i++) {
-			String result = ExcelUtils.readTwoContentAndJudge(excel,i, beginCell,endCell);
-			if (result!=null) {
-				ExcelUtils.writeAndSaveContent(excel,result,i,writeCell);
-			}
-		}
-		return "比较完成。。。";
-	}
-
-	
 }

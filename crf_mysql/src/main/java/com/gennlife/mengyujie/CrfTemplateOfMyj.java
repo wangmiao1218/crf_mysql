1327350286a5d105f17b76258e48bdc83af47c75
wangmiao1218 <wangmiao1218@163.com>
2017-08-30 10:42:31 +0800

    增加 crf模板自动配置映射路径

---
+++ crf_mysql/src/main/java/com/gennlife/mengyujie/CrfTemplateOfMyj.java
@@ -0,0 +1,14 @@
+package com.gennlife.mengyujie;
+
+
+/**
+ * @Description: 
+ * @author: wangmiao
+ * @Date: 2017年8月30日 上午9:16:08 
+ */
+public class CrfTemplateOfMyj{
+
+	
+	
+	
+}
---
+++ crf_mysql/src/test/java/com/gennlife/mengyujie/test/TestCrfTemplateOfMyj.java
@@ -0,0 +1,21 @@
+package com.gennlife.mengyujie.test;
+
+import org.junit.Test;
+
+public class TestCrfTemplateOfMyj {
+
+	private String filePath = "E:\\yujie";
+	private String fileName = "英文版乳腺癌2.xlsx";
+	private String sheetName = "就诊-介入治疗(package)";
+	
+	private String fileName2 = "CRF表字段配置2.xlsx";
+	
+	@Test
+	public void testYujie(){
+		
+		System.out.println("ok");
+	}
+	
+	
+	
+}

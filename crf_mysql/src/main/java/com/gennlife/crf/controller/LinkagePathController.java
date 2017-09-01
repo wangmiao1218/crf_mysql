2cb00939cb8e7819ef6079a494600101ebacf73d
wangmiao1218 <wangmiao1218@163.com>
2017-09-01 18:24:19 +0800

    add linux filePath

--- crf_mysql/src/main/java/com/gennlife/crf/controller/LinkagePathController.java
+++ crf_mysql/src/main/java/com/gennlife/crf/controller/LinkagePathController.java
@@ -1,23 +1,16 @@
 package com.gennlife.crf.controller;
 
 import java.io.File;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.collections4.map.HashedMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.gennlife.crf.bean.ResultBean;
-import com.gennlife.crf.bean.SysOp;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 
 @Controller
 @RequestMapping("linkagePathController")
@@ -25,9 +18,16 @@ public class LinkagePathController {
 	
 	@RequestMapping("uploadFile")
 	@ResponseBody
-	public ResultBean uploadFile(MultipartFile uploadFile) throws Exception{
+	public ResultBean uploadFile(MultipartFile uploadFile,HttpServletResponse response, HttpSession session) throws Exception{
 		ResultBean result = new ResultBean();
-		// 1、指定一个文件夹用来存放上传的文件
+		
+		// linux上运行
+		// 获得服务器应用程序所在的绝对路径
+		
+		
+		
+		// windows上运行
+		//1、指定一个文件夹用来存放上传的文件
 		String path = "F:\\test\\";
 		File filePath = new File(path);
 		// 2、如果不存在该文件夹，就创建

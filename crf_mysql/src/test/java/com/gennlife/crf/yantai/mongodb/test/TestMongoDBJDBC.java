245a8534f3f09dc74620a823b28c8e5f67ed9f9b
wangmiao1218 <wangmiao1218@163.com>
2017-09-14 18:26:46 +0800

    add query mongodb

--- crf_mysql/src/test/java/com/gennlife/crf/yantai/mongodb/test/TestMongoDBJDBC.java
+++ crf_mysql/src/test/java/com/gennlife/crf/yantai/mongodb/test/TestMongoDBJDBC.java
@@ -3,6 +3,7 @@ package com.gennlife.crf.yantai.mongodb.test;
 import org.bson.BSONObject;
 import org.bson.Document;
 import org.junit.Test;
+
 import com.gennlife.crf.utils.MongoDBJDBC;
 import com.mongodb.BasicDBObject;
 import com.mongodb.Block;
@@ -21,10 +22,19 @@ public class TestMongoDBJDBC {
 		MongoCollection<Document> collection = yantaiMongodb.getCollection("patientDetail");
 		System.out.println("集合 patientDetail 选择成功");
 		
+		//打的查询条件
 		BasicDBObject query = new BasicDBObject();  
 		query.put("patient_info.patient_info_patient_sn","pat_60fc1ba93ac2f03de9f2206e38b52669"); 
 		
+		//数组内查询条件
+		BasicDBObject edit = new BasicDBObject();
+		edit.put("type.visit_info_admission_date","2016-08-05 10:59:00");
+		edit.put("type.visit_info_admission_dept","神经内科");
 		
+		query.put("visits", new BasicDBObject("$elemMatch", edit));
+		
+		//query.put("visits", 1);
+
 		
 		FindIterable<Document> iterable = collection.find(query);
 		iterable.forEach(new Block<Document>() { 

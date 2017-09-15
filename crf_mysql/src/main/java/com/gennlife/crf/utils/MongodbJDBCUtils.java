e6f3f78215e3943d10b4ac683a31c441ea66296b
wangmiao1218 <wangmiao1218@163.com>
2017-09-15 11:01:09 +0800

    add MongodbJDBCUtils

---
+++ crf_mysql/src/main/java/com/gennlife/crf/utils/MongodbJDBCUtils.java
@@ -0,0 +1,95 @@
+package com.gennlife.crf.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * @Description: 连接mongodb
+ * @author: wangmiao
+ * @Date: 2017年9月14日 下午4:27:55
+ */
+public class MongodbJDBCUtils {
+
+	/** 
+	* @Title: connectYantaiMongodbReturnMongoCollection 
+	* @Description: 连接烟台测试环境mongodb数据库,返回MongoCollection<Document>
+	* @param: @return :
+	* @return: MongoCollection<Document> 返回 MongoCollection<Document>
+	* @throws 
+	*/
+	public static MongoCollection<Document> connectYantaiMongodbReturnMongoCollection() {
+		MongoCollection<Document> mongoCollection = null;
+		try {
+			// 连接到MongoDB服务 如果是远程连接可以替换“localhost”为服务器所在IP地址
+			// ServerAddress()两个参数分别为 服务器地址 和 端口
+			ServerAddress serverAddress = new ServerAddress("10.0.2.171", 27017);
+			List<ServerAddress> addrs = new ArrayList<ServerAddress>();
+			addrs.add(serverAddress);
+
+			// MongoCredential.createScramSha1Credential()三个参数分别为 用户名 数据库名称 密码
+			MongoCredential credential = MongoCredential.createScramSha1Credential("wangmiao", "CRF_Model","@wangmiao2015".toCharArray());
+			List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+			credentials.add(credential);
+
+			// 通过连接认证获取MongoDB连接
+			MongoClient mongoClient = new MongoClient(addrs, credentials);
+
+			// 连接到数据库
+			MongoDatabase mongoDatabase = mongoClient.getDatabase("CRF_Model");
+			System.out.println("Connect to database successfully");
+			
+			//获取集合
+			mongoCollection = mongoDatabase.getCollection("patientDetail");
+			
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return mongoCollection;
+
+	}
+	
+	
+	/** 
+	* @Title: connectYantaiMongodbReturnDBCollection 
+	* @Description: 连接烟台测试环境mongodb数据库,返回DBCollection
+	* (官方文档和源代码均建议使用MongoClient类，而且，在不久的将来，会废弃Mongo类。)
+	* @param: @return :
+	* @return: DBCollection
+	* @throws 
+	*/
+	public static DBCollection connectYantaiMongodbReturnDBCollection() {
+		DBCollection dbCollection =null;
+		try {
+			MongoCredential credential = MongoCredential.createCredential("wangmiao", "CRF_Model", "@wangmiao2015".toCharArray()); 
+			ServerAddress serverAddress = new ServerAddress("10.0.2.171", 27017);
+			MongoClient mongoClient = new MongoClient(serverAddress, Arrays.asList(credential)); 
+	
+			// 连接到数据库
+			DB db = mongoClient.getDB("CRF_Model");
+			System.out.println("Connect to database successfully");
+			
+			//获取集合
+			dbCollection = db.getCollection("patientDetail");
+			
+		}catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		return dbCollection;
+	}
+
+	
+	
+}
---
+++ crf_mysql/src/test/java/com/gennlife/crf/yantai/mongodb/test/TestMongodbJDBCUtils.java
@@ -0,0 +1,69 @@
+package com.gennlife.crf.yantai.mongodb.test;
+
+import org.bson.BSONObject;
+import org.bson.Document;
+import org.junit.Test;
+
+import com.gennlife.crf.utils.MongodbJDBCUtils;
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+
+public class TestMongodbJDBCUtils {
+
+	@Test
+	public void connectYantaiMongodbReturnMongoCollection() {
+		MongoCollection<Document> mongoCollection = MongodbJDBCUtils.connectYantaiMongodbReturnMongoCollection();
+		//大的查询条件
+		BasicDBObject query = new BasicDBObject();  
+		query.put("patient_info.patient_info_patient_sn","pat_60fc1ba93ac2f03de9f2206e38b52669"); 
+		
+		//数组内查询条件
+		BasicDBObject edit = new BasicDBObject();
+		edit.put("type.visit_info_admission_date","2016-08-05 10:59:00");
+		edit.put("type.visit_info_admission_dept","神经内科");
+		
+		query.put("visits", new BasicDBObject("$elemMatch",edit));
+		
+		FindIterable<Document> iterable = mongoCollection.find(query);
+		
+		iterable.forEach(new Block<Document>() { 
+			@Override public void apply(final Document document) { 
+				//System.out.println(document); 
+				System.out.println(document.toJson());
+			}
+		});
+		
+	}
+	
+	
+	@Test
+	public void connectYantaiMongodbReturnDBCollection() {
+		DBCollection dbCollection = MongodbJDBCUtils.connectYantaiMongodbReturnDBCollection();
+		
+		//大的查询条件
+		BasicDBObject query = new BasicDBObject();  
+		query.put("patient_info.patient_info_patient_sn","pat_60fc1ba93ac2f03de9f2206e38b52669"); 
+		
+		//数组内查询条件
+		BasicDBObject edit = new BasicDBObject();
+		edit.put("visits", new BasicDBObject("$elemMatch", 
+				new BasicDBObject("type.visit_info_admission_date","2016-08-05 10:59:00")
+				.append("type.visit_info_admission_dept","神经内科")));
+		
+        DBCursor cursor=dbCollection.find(query,edit);
+        try {
+            while (cursor.hasNext()) {
+            	BSONObject dbObject = cursor.next();
+            	System.out.println(dbObject);
+            }
+        } finally {
+            cursor.close();
+        }
+	}
+	
+	
+}

c77e5a82561736c7e9728ab9d9fb674292acada8
wangmiao1218 <wangmiao1218@163.com>
2017-09-22 15:27:08 +0800

    update anzhenlogin

--- crf_mysql/src/main/java/com/gennlife/crf/utils/LoginCrfOfAnzhen.java
+++ crf_mysql/src/main/java/com/gennlife/crf/utils/LoginCrfOfAnzhen.java
@@ -29,104 +29,6 @@ public class LoginCrfOfAnzhen {
 	public static final String pwd ="ls123456";
 	
 	/**
-	 * @Title: loginByChromeWebDriver
-	 * @Description: 仅登录,通过chrome驱动，有页面显示的方式
-	 * @param: WebDriver driver :传入driver
-	 * @return: String： 返回，登录成功返回“单病种数据库”
-	 * @throws
-	 */
-	public static String loginByChromeWebDriver(WebDriver driver) {
-		driver.navigate().to("http://119.253.137.125/uranus/login.html");
-
-		// 获取 网页的 title
-		System.out.println("title is: " + driver.getTitle());
-
-		// 登录
-		WebElement loginName = driver.findElement(By.xpath("/html/body/div[1]/form/div/input[1]"));
-		WebElement pwd = driver.findElement(By.xpath("/html/body/div[1]/form/div/input[2]"));
-
-		loginName.sendKeys("wangmiao@gennlife.com");
-		pwd.sendKeys("ls123456");
-		driver.findElement(By.xpath("/html/body/div[1]/form/div/button")).click();
-
-		// 等待
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		String returnValue = null;
-
-		// 获取单病种标题
-		String text = driver.findElement(By.xpath("/html/body/div/div[1]/nav/div[2]/ul/li[5]/a")).getText();
-		//
-		if ("单病种数据库".equals(text)) {
-			returnValue = text;
-		}
-		return returnValue;
-
-	}
-
-	/**
-	 * @Title: loginAndToAddByChromeWebDriver
-	 * @Description: 登录并到添加页面，通过chrome驱动，有页面显示的方式
-	 * @param: WebDriver driver :传入driver
-	 * @return: String： 返回，登录并到添加页面，成功返回“添加页面”
-	 * @throws
-	 */
-	public static String loginAndToAddByChromeWebDriver(WebDriver driver) {
-		String returnValue = loginByChromeWebDriver(driver);
-
-		if ("单病种数据库".equals(returnValue)) {
-			// 获取添加按钮
-			driver.findElement(By.xpath("/html/body/div/div[3]/div/div[2]/div/div[1]/button[1]")).click();
-
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-			// 获取基线_门诊
-			driver.findElement(By.xpath("/html/body/div[1]/div[8]/div/div/div/div[2]/a[1]")).click();
-
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-			// 得到当前窗口的set集合
-			Set<String> winHandels = driver.getWindowHandles();
-
-			// 将set集合存入list对象
-			List<String> it = new ArrayList<String>(winHandels);
-
-			// 切换到弹出的新窗口
-			driver.switchTo().window(it.get(1));
-		}
-		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		String value = null;
-
-		// 获取保存按钮，若存在则在添加页面
-		String text = driver.findElement(By.xpath("html/body/div[2]/div[2]/div[1]/div[2]/button[2]")).getText();
-		//
-		if ("保存".equals(text)) {
-			value = "添加页面";
-		}
-		
-		return value;
-	}
-
-	
-	/**
 	 * @Title: loginByPhantomJSDriver
 	 * @Description: 仅登录,通过PhantomJSDriver，无页面显示的方式
 	 * @param: PhantomJSDriver driver :传入PhantomJSDriver
@@ -231,7 +133,6 @@ public class LoginCrfOfAnzhen {
 		
 		return value;
 	}
-	
 	
 	
 	
@@ -465,5 +366,103 @@ public class LoginCrfOfAnzhen {
 		return value;
 	}
 	
+
+	/**
+	 * @Title: loginByChromeWebDriver
+	 * @Description: 仅登录,通过chrome驱动，有页面显示的方式
+	 * @param: WebDriver driver :传入driver
+	 * @return: String： 返回，登录成功返回“单病种数据库”
+	 * @throws
+	 */
+	public static String loginByChromeWebDriver(WebDriver driver) {
+		driver.navigate().to("http://119.253.137.125/uranus/login.html");
+
+		// 获取 网页的 title
+		System.out.println("title is: " + driver.getTitle());
+
+		// 登录
+		WebElement loginName = driver.findElement(By.xpath("/html/body/div[1]/form/div/input[1]"));
+		WebElement pwd = driver.findElement(By.xpath("/html/body/div[1]/form/div/input[2]"));
+
+		loginName.sendKeys("wangmiao@gennlife.com");
+		pwd.sendKeys("ls123456");
+		driver.findElement(By.xpath("/html/body/div[1]/form/div/button")).click();
+
+		// 等待
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		String returnValue = null;
+
+		// 获取单病种标题
+		String text = driver.findElement(By.xpath("/html/body/div/div[1]/nav/div[2]/ul/li[5]/a")).getText();
+		//
+		if ("单病种数据库".equals(text)) {
+			returnValue = text;
+		}
+		return returnValue;
+
+	}
+
+	/**
+	 * @Title: loginAndToAddByChromeWebDriver
+	 * @Description: 登录并到添加页面，通过chrome驱动，有页面显示的方式
+	 * @param: WebDriver driver :传入driver
+	 * @return: String： 返回，登录并到添加页面，成功返回“添加页面”
+	 * @throws
+	 */
+	public static String loginAndToAddByChromeWebDriver(WebDriver driver) {
+		String returnValue = loginByChromeWebDriver(driver);
+
+		if ("单病种数据库".equals(returnValue)) {
+			// 获取添加按钮
+			driver.findElement(By.xpath("/html/body/div/div[3]/div/div[2]/div/div[1]/button[1]")).click();
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			// 获取基线_门诊
+			driver.findElement(By.xpath("/html/body/div[1]/div[8]/div/div/div/div[2]/a[1]")).click();
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			// 得到当前窗口的set集合
+			Set<String> winHandels = driver.getWindowHandles();
+
+			// 将set集合存入list对象
+			List<String> it = new ArrayList<String>(winHandels);
+
+			// 切换到弹出的新窗口
+			driver.switchTo().window(it.get(1));
+		}
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		String value = null;
+
+		// 获取保存按钮，若存在则在添加页面
+		String text = driver.findElement(By.xpath("html/body/div[2]/div[2]/div[1]/div[2]/button[2]")).getText();
+		//
+		if ("保存".equals(text)) {
+			value = "添加页面";
+		}
+		
+		return value;
+	}
+
 	
 }

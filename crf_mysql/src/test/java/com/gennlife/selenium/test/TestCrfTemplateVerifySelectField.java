d6f0aaf44652a603e62ae27496623e8be9edbe9b
wangmiao1218 <wangmiao1218@163.com>
2017-09-22 12:08:00 +0800

    add  TestCrfTemplateVerifySelectField

--- crf_mysql/src/test/java/com/gennlife/selenium/test/TestCrfTemplateVerifySelectField.java
+++ crf_mysql/src/test/java/com/gennlife/selenium/test/TestCrfTemplateVerifySelectField.java
@@ -3,9 +3,9 @@ package com.gennlife.selenium.test;
 import org.junit.Test;
 
 import com.gennlife.crf.bean.Excel;
-import com.gennlife.selenium.CrfTemplateVerifyLinkageField;
+import com.gennlife.selenium.CrfTemplateVerifySelectField;
 
-public class TestCrfTemplateVerifyLinkageField {
+public class TestCrfTemplateVerifySelectField {
 
 	private String filePath = "E:\\安贞\\！安贞心血管\\test\\excel";
 	private String fileName = "test-模板结构-安贞高血压.xlsx";
@@ -14,12 +14,11 @@ public class TestCrfTemplateVerifyLinkageField {
 	private String sheetName = "总体结构";
 	
 	@Test
-	public void verifyLinkageField() throws Exception{
+	public void verifySelectField() throws Exception{
 		Excel excelmb = new Excel(filePath, fileName, sheetName);
 		Excel excel = new Excel(filePath, fileName2, sheetName);
-		CrfTemplateVerifyLinkageField.verifyLinkageField(excelmb, excel);
+		CrfTemplateVerifySelectField.verifySelectField(excelmb, excel);
 		System.out.println("ok");
 	}
-	
 	
 }

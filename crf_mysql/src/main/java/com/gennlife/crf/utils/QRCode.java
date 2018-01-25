12478f377759ad05e497982f43ef68d8f1968631
wangmiao1218 <wangmiao1218@163.com>
2018-01-25 17:16:11 +0800

    增加二维码生成

---
+++ crf_mysql/src/main/java/com/gennlife/crf/utils/QRCode.java
@@ -0,0 +1,37 @@
+package com.gennlife.crf.utils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Hashtable;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class QRCode {
+
+	public static void main(String[] args) throws IOException {
+		String text = "你好";
+		int width = 100;
+		int height = 100;
+		String format = "png";
+		Hashtable hints = new Hashtable();
+		hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+		hints.put(EncodeHintType.MARGIN, 2);
+		try {
+			BitMatrix bitMatrix = new MultiFormatWriter().encode(text,
+					BarcodeFormat.QR_CODE, width, height, hints);
+			Path file = new java.io.File("E:/new.png").toPath();
+			MatrixToImageWriter.writeToPath(bitMatrix, format, file);
+
+		} catch (WriterException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

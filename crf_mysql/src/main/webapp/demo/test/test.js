ceb11b2d99ca72babe5f3772ea327770220d41ba
wangmiao1218 <wangmiao1218@163.com>
2018-03-15 15:32:21 +0800

    highcharts的demo测试

--- crf_mysql/src/main/webapp/demo/test/test.js
+++ crf_mysql/src/main/webapp/demo/test/test.js
@@ -25,6 +25,10 @@ function addLineChart(globalObj, elementID, title, subTitle, categories, xName,
 			},
 			x : -20
 		},
+		subtitle: {
+            text: document.ontouchstart === undefined ?
+            '鼠标拖动可以进行缩放' : '手势操作进行缩放'
+        }, 
 		xAxis : {
 			title : {
 				text : xName
@@ -64,57 +68,3 @@ function addLineChart(globalObj, elementID, title, subTitle, categories, xName,
 		series : seriesArr
 	});
 }
-
-//////////////////////////////////////////////////////////////
-
-var chart1; // 全局变量
-$(document).ready(function() {    
-    var totalNum = $("#totalNum").val();//页面数据记录条数
-    var st1=new Array();
-    for(var i=totalNum-1;i>=0;i--){//组装数组数据
-        var st2=new Array();
-        var tt = $("#data"+i).val();
-        var ttt = $("#Daily"+i).val();
-        st2.push(Date.UTC(tt.split("-")[0],tt.split("-")[1]-1,tt.split("-")[2]));
-        st2.push(parseInt(ttt));
-        st1.push(st2);
-    }
-    chart1 = new Highcharts.Chart({
-         credits: {  enabled: false},//去掉highcharts.com商标
-         exporting: { enabled: false },  //去掉chart不必要属性
-         chart: { renderTo: 'container', type: 'line' },
-         title: { text: 'xxxx' },
-         tooltip: {
-              xDateFormat: '%Y-%m-%d, %A'//鼠标移动到趋势线上时显示的日期格式
-         },
-         xAxis: {type: 'datetime',
-             dateTimeLabelFormats : { 
-                day : '%m-%d',
-                //second: '%H:%M:%S',
-                   // minute: '%e. %b %H:%M',
-                  //  hour: '%b/%e %H:%M',
-                  //  day: '%e日/%b',
-                   // week: '%e. %b',
-                   // month: '%b %y',
-                   // year: '%Y'
-            },
-            tickInterval: 2*24 * 3600 * 1000//间隔2天
-        },
-         yAxis: {title: { text: 'xxx', }, min:'0' },
-         series: [{
-            name: 'xxx',
-            //用的数组格式
-            data:st1
-}] }); });
-
-
-
-
-
-
-
-
-
-
-
-

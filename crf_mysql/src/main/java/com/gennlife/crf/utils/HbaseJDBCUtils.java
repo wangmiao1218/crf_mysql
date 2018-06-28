59b142bb65584db3172617890bb6b4bb185d098c
wangmiao1218 <wangmiao1218@163.com>
2018-06-28 13:52:35 +0800

    test

--- crf_mysql/src/main/java/com/gennlife/crf/utils/HbaseJDBCUtils.java
+++ crf_mysql/src/main/java/com/gennlife/crf/utils/HbaseJDBCUtils.java
@@ -1,4 +1,15 @@
 package com.gennlife.crf.utils;
+/*
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+*/
+
+import java.io.Serializable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * @Description: 
@@ -7,6 +18,8 @@ package com.gennlife.crf.utils;
  */
 public class HbaseJDBCUtils {
 	
+	public static void main(String[] args) {
+	}
     // 用HBaseconfiguration初始化配置信息是会自动加载当前应用的classpath下的hbase-site.xml
    /* public static Configuration configuration = HBaseConfiguration.create();
 
@@ -16,4 +29,17 @@ public class HbaseJDBCUtils {
         // configuration.addResource("hbase-site.xml");
         connection = ConnectionFactory.createConnection(configuration);
     }*/
+	
+/*
+    private ConcurrentLinkedQueue<Connection> connections = new ConcurrentLinkedQueue<Connection>();
+    private Connection connection;
+    public Connection getHbaseConnection() throws Exception{
+        Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.zookeeper.quorum", "hadoop1.in.gennlife.cn,hadoop2.in.gennlife.cn,hadoop3.in.gennlife.cn");
+        conf.set("hbase.zookeeper.property.clientPort", "2181");
+        connection = ConnectionFactory.createConnection(conf);
+        System.out.println("==========");
+	        return connection;
+	}*/
+    
 }

95c5fff0621e3ee7ab98ef436c15e64cec5578c4
wangmiao1218 <wangmiao1218@163.com>
2018-09-01 12:00:05 +0800

    test

---
+++ crf_mysql/src/main/java/test/Test001.java
@@ -0,0 +1,19 @@
+package test;
+
+
+public class Test001 {
+	public static void main(String[] args) throws Exception {
+	
+		String path = "E:\\test\\a a\\a a.txt";
+		String urlString = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+		String urlString1 = "C:\\Program Files (x86)\\Google\\Chrome\\Application";
+		//Runtime.getRuntime().exec("cmd /c start " + "\"\" \"" + urlString + "\" \"http://10.0.2.162/uranus/search_index.html\"");
+		//Runtime.getRuntime().exec("cmd /c start " + "\"\" \"" + urlString1+"\\chrome.exe" + "\" \"http://10.0.2.162/uranus/search_index.html\"");
+		Runtime.getRuntime().exec("cmd /c start " + "\"\" \"" + urlString1+"\\chrome.exe\" \"http://10.0.2.162/uranus/search_index.html\"");
+		
+		
+		
+		//Runtime.getRuntime().exec("cmd /c start E:\\test\\a~1\\a a.txt");
+	}
+
+}

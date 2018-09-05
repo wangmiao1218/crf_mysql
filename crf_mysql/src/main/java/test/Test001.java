91a567f7d6042b8e057d0259d83e6a2f2643c707
wangmiao1218 <wangmiao1218@163.com>
2018-09-05 17:04:28 +0800

    test

--- crf_mysql/src/main/java/test/Test001.java
+++ crf_mysql/src/main/java/test/Test001.java
@@ -11,7 +11,6 @@ public class Test001 {
 		//Runtime.getRuntime().exec("cmd /c start " + "\"\" \"" + urlString1+"\\chrome.exe" + "\" \"http://10.0.2.162/uranus/search_index.html\"");
 		Runtime.getRuntime().exec("cmd /c start " + "\"\" \"" + urlString1+"\\chrome.exe\" \"http://10.0.2.162/uranus/search_index.html\"");
 		
-		
 		//Runtime.getRuntime().exec("cmd /c start E:\\test\\a~1\\a a.txt");
 	}
 
